a599d7b01b7743b8948ac8107e99cd2e8e35feca
Ryan Morehart <ryan@moreharts.com>
2012-08-25 16:40:51 -0400

    ADDR_SIZE actually comes from types now, rather than being hardcoded

--- net_info.h
+++ net_info.h
@@ -1,13 +1,15 @@
 #ifndef NET_INFO_H
 #define NET_INFO_H
 
+#include <linux/types.h>
+
 // Protocol numbers
 #define ICMP_PROTO 0x01
 #define TCP_PROTO 0x06
 #define UDP_PROTO 0x11
 
 // Size of IP addresses (bytes)
-#define ADDR_SIZE 4
+#define ADDR_SIZE sizeof(__be32)
 
 #endif
 
